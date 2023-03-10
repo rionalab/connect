6a9b95fb074d1a4d40569b1c820dc0bf75111af8
selda <griselda.silviyana@idxpartners.com>
2023-03-10 13:41:05 +0700

    add header

--- src/comps/Header/index.js
+++ src/comps/Header/index.js
@@ -1,7 +1,26 @@
 import React from "react";
+import style from "./style.module.scss";
 
 function Header() {
-  return <div>Header</div>;
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const date = new Date().toLocaleDateString("en-US", options);
+
+  return (
+    <div className={style.header}>
+      <div className={style.content}>
+        <p>{date}</p>
+
+        <img src="/logo.png" />
+
+        <button>Login</button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
