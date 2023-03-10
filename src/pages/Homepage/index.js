b7f91b2a4d85b52a19a7d1800f0670526d6dbe4e
rionalab <rionalab@gmail.com>
2023-03-10 12:51:11 +0700

    update

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,8 +1,13 @@
+import Header from "comps/Header";
 import React from "react";
 import style from "./style.module.scss";
 
 function Homepage() {
-  return <div className={style.a}>Homepage</div>;
+  return (
+    <div className={`pageContainer ${style.a}`}>
+      <Header />
+    </div>
+  );
 }
 
 export default Homepage;
