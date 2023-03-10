d70d86f7b28538b519675ccac7b6ace6175e7044
selda <griselda.silviyana@idxpartners.com>
2023-03-10 15:57:18 +0700

    add birthday dialog

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import Homepage from "pages/Homepage";
 import Login from "pages/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,6 +1,7 @@
 import Header from "comps/Header";
 import Ticker from "comps/Ticker";
 import React from "react";
+import BirthdayModal from "comps/modals/BIrthdayModal";
 import style from "./style.module.scss";
 
 function Homepage() {
@@ -8,6 +9,7 @@ function Homepage() {
     <div className={`pageContainer ${style.a}`}>
       <Header />
       <Ticker />
+      <BirthdayModal />
     </div>
   );
 }
