2aa0ec6f3432c10095e5f64421292953d612b800
selda <griselda.silviyana@idxpartners.com>
2023-03-20 15:33:43 +0700

    parse fake kurs.xml

--- src/comps/Ticker/index.js
+++ src/comps/Ticker/index.js
@@ -4,14 +4,26 @@ import dataKurs from "data/kurs.json";
 import { Dialog } from "primereact/dialog";
 import Marquee from "react-fast-marquee";
 
-function Ticker() {
-  const { data } = dataKurs;
+function Ticker(props) {
+  const { data, getKurs } = props;
   const [visible, setVisible] = useState(false);
-  const [row, setRow] = useState([]);
+  const flag = [
+    "USD",
+    "SGD",
+    "AUD",
+    "EUR",
+    "GBD",
+    "CAD",
+    "CHF",
+    "HKD",
+    "JPY",
+    "SAR",
+    "MYR",
+    "THB",
+    "NZD",
+  ];
 
-  useEffect(() => {
-    setRow(dataKurs.data.map((row) => row.counter.map((row2) => row2)));
-  }, []);
+  // console.log(data);
 
   return (
     <>
@@ -23,95 +35,114 @@ function Ticker() {
         className={style.modal}
         onHide={() => setVisible(false)}
       >
-        <p className="m-0">
-          <table cellSpacing={0} className={style.tbl}>
-            <thead>
-              <tr>
-                <th align="left">Mata Uang</th>
-                <th align="right">Beli</th>
-                <th align="right">Jual</th>
-              </tr>
-            </thead>
-            <tbody>
-              {row.map((tr) =>
-                tr.map((tr2) => (
-                  <tr key={tr2.name}>
+        {/* <p className="m-0"> */}
+        <table cellSpacing={0} className={style.tbl}>
+          <thead>
+            <tr>
+              <th align="left">Mata Uang</th>
+              <th align="right">Beli</th>
+              <th align="right">Jual</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data?.currency?.map((tr, i) => {
+              if (tr.code2 === "IDR" && flag.includes(tr.code)) {
+                return (
+                  <tr key={i}>
                     <td>
                       <p>
-                        <img alt={tr2.name} src={`/images/${tr2.name}.jpg`} />
-                        {tr2.name}
+                        <img alt={tr.code} src={`/images/${tr.code}.jpg`} />
+                        {tr.code}
                       </p>
                     </td>
-                    <td align="right">{tr2.beli}</td>
-                    <td align="right">{tr2.jual}</td>
+                    <td align="right">
+                      {Math.round(Number(tr.buyRate * 100) / 100).toFixed(2)}
+                    </td>
+                    <td align="right">
+                      {Math.round(Number(tr.sellRate * 100) / 100).toFixed(2)}
+                    </td>
                   </tr>
-                ))
-              )}
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colSpan={3}>
-                  *Pemberitahuan Terakhir: 22-03-2023 14:20 WIB (GMT+07:00)
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-        </p>
+                );
+              }
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={3}>
+                *Pemberitahuan Terakhir: {data?.currencyVersion}
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+        {/* </p> */}
       </Dialog>
 
       <div className={style.tickerContainer}>
         <div>
           <h3 title="Last update at 22/03/2023" className={style.title}>
-            Kurs TT Counter [22/03 14:20 WIB]
+            Kurs TT Counter [
+            {data?.currencyVersion.split(" ")[0].split("-")[1] +
+              "/" +
+              data?.currencyVersion.split(" ")[0].split("-")[2] +
+              " " +
+              data?.currencyVersion.split(" ")[1].split(":")[0] +
+              ":" +
+              data?.currencyVersion.split(" ")[1].split(":")[1] +
+              " " +
+              data?.currencyVersion.split(" ")[2]}
+            ]
           </h3>
         </div>
 
         {true && (
           <div className={style.marquee}>
             <Marquee speed={50} gradient={false} pauseOnHover={true}>
-              {data.map((row, i) => {
-                return (
-                  <div key={i} className={style.tickerItems}>
-                    {row.counter.map((row2, i) => {
-                      return (
-                        <div className={style.tickerItem} key={row2.name}>
+              <div className={style.tickerItems}>
+                {data?.currency?.map((row, i) => {
+                  if (row.code2 === "IDR" && flag.includes(row.code)) {
+                    return (
+                      <div className={style.tickerItem} key={i}>
+                        <div>
+                          {/* <div className={style.flag}> */}
+                          <div>{row.code}</div>
                           <div>
-                            <div className={style.flag}>
-                              <div>{row2.name}</div>
-                              <div>
-                                <img
-                                  src={`/images/${row2.name}.jpg`}
-                                  alt={row2.name}
-                                />
-                              </div>
-                            </div>
-                            <div>
-                              <span className={style.sell}>
-                                <span>Sell</span>
-                                <br />
-                                {row2.jual.split(",")[0]}
-                              </span>
-                            </div>
-                            <div>
-                              <span className={style.buy}>
-                                <span>Buy</span>
-                                <br />
-                                {row2.beli.split(",")[0]}
-                              </span>
-                            </div>
+                            <img
+                              src={`/images/${row.code}.jpg`}
+                              alt={row.code}
+                            />
+                          </div>
+                          {/* </div> */}
+                          <div>
+                            <span className={style.sell}>
+                              <span>Sell</span>
+                              <br />
+                              {Math.round(
+                                Number(row.sellRate * 100) / 100
+                              ).toFixed(2)}
+                            </span>
+                          </div>
+                          <div>
+                            <span className={style.buy}>
+                              <span>Buy</span>
+                              <br />
+                              {Math.round(
+                                Number(row.buyRate * 100) / 100
+                              ).toFixed(2)}
+                              {/* {row.buyRate.split(",")[0]} */}
+                            </span>
                           </div>
                         </div>
-                      );
-                    })}
-                  </div>
-                );
-              })}
+                      </div>
+                    );
+                  }
+                })}
+              </div>
             </Marquee>
           </div>
         )}
 
         <div className={style.button}>
-          <button className={style.refresh}>
+          <button className={style.refresh} onClick={getKurs}>
             <i className="pi pi-refresh"></i>
           </button>
 
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -4,9 +4,10 @@ import BodyTop from "comps/BodyTop";
 import React from "react";
 import style from "./style.module.scss";
 import Footer from "comps/Footer";
-import { parseString } from "xml2js";
+import { parseString, processors, Builder } from "xml2js";
 
 function Homepage() {
+  const [dataKurs, setDataKurs] = React.useState(null);
   const url =
     "https://news.google.com/rss/search?q=Industri+Mekanik+Elektrik&hl=id&gl=ID&ceid=ID:id";
   // "https://www.youtube.com/feeds/videos.xml?channel_id=UC4woSp8ITBoYDmjkukhEhxg";
@@ -19,19 +20,18 @@ function Homepage() {
     xmlhttp.open("POST", urlKurs, true);
 
     // build SOAP request
-    var sr =
-      '<soapenv:Envelope xmlns:q0="http://service.bni.co.id/core" xmlns:bo="http://service.bni.co.id/core/bo" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">' +
-      "<soapenv:Body>" +
-      "<q0:transaction>" +
-      "<request>" +
-      "<systemId>XRATE</systemId>" +
-      '<content xsi:type="bo:ExchangeRateByTypeReq">' +
-      "<rateType>02</rateType>" +
-      "</content>" +
-      "</request>" +
-      "</q0:transaction>" +
-      "</soapenv:Body>" +
-      "</soapenv:Envelope>";
+    var sr = `<soapenv:Envelope xmlns:q0="http://service.bni.co.id/core" xmlns:bo="http://service.bni.co.id/core/bo" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <soapenv:Body>
+       <q0:transaction>
+          <request>
+             <systemId>XRATE</systemId>
+             <content xsi:type="bo:ExchangeRateByTypeReq">
+                <rateType>02</rateType>
+             </content>
+          </request>
+       </q0:transaction>
+    </soapenv:Body>
+ </soapenv:Envelope>`;
 
     xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState == 4) {
@@ -57,15 +57,43 @@ function Homepage() {
       .catch((err) => console.log(err));
   };
 
+  const getKurs2 = async () => {
+    await fetch("kurs.xml")
+      .then((res) => res.text())
+      .then((data) =>
+        parseString(
+          data,
+          { explicitArray: false, tagNameProcessors: [processors.stripPrefix] },
+          (err, res) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+
+            var soapBody =
+              res.Envelope.Body.transactionResponse.response.content;
+
+            if (soapBody.$) {
+              delete soapBody.$;
+            }
+
+            setDataKurs(soapBody);
+          }
+        )
+      )
+      .catch((err) => console.log(err));
+  };
+
   React.useEffect(() => {
     getFeed();
-    getKurs();
+    // getKurs();
+    getKurs2();
   }, []);
 
   return (
     <div className={`pageContainer ${style.a}`}>
       <Header />
-      <Ticker />
+      <Ticker data={dataKurs} getKurs={getKurs2} />
       <BodyTop />
       <Footer />
     </div>
